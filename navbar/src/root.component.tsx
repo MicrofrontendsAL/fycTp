213b33e614a843d05f3a483234d58d364c0f68ee
David <darnaud2@myges.fr>
2023-01-15 16:44:15 +0100

    fix navbar

--- navbar/src/root.component.tsx
+++ navbar/src/root.component.tsx
@@ -1,3 +1,11 @@
+import './navbar.css'
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+      <div className="component--navbar">
+          <div className="navbar">
+              <div className="title-navbar">Trains Ticket</div>
+              <a href="/dc" className="link">Panier</a>
+          </div>
+      </div>
+  )
 }
